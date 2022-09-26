52294eab711e2c71bca7a1e47cf5b0c8be304a0e
Noel S <sollberger.n@gmail.com>
2022-09-26 11:08:25 +0000

    added clang-format

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-
-  public static void main(String[] args) {
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
     // Ausgabe Hello World!
     System.out.println("Hello World!");
   }
